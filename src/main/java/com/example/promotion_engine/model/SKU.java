dab91671f1aa0819728428bca67cef5887dea763
AnkeshKr789 <ankeshkumar7890@gmail.com>
2025-07-06 09:15:40 +0530

    feat: add SKU java file

---
+++ src/main/java/com/example/promotion_engine/model/SKU.java
@@ -0,0 +1,11 @@
+package com.example.promotion_engine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SKU {
+    private String id;
+    private int unitPrice;
+}
