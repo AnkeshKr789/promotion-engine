98ded4a6b063f9a0002584166befed3763c1d9be
AnkeshKr789 <ankeshkumar7890@gmail.com>
2025-07-06 10:48:10 +0530

    feat: add Service TestScenario A

---
+++ src/test/java/com/example/promotion_engine/service/PromotionEngineServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.promotion_engine.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.promotion_engine.config.SkuConfig;
+import com.example.promotion_engine.model.CartItem;
+import com.example.promotion_engine.model.CartRequest;
+import com.example.promotion_engine.promotion.PromotionFactory;
+
+public class PromotionEngineServiceTest {
+    private PromotionEngineService promotionEngineService;
+
+    @BeforeEach
+    void setup() {
+        SkuConfig skuConfig = new SkuConfig();
+        skuConfig.setPrices(Map.of("A", 50, "B", 30, "C", 20, "D", 15));
+        PromotionFactory factory = new PromotionFactory(skuConfig);
+        promotionEngineService = new PromotionEngineService(skuConfig, factory);
+    }
+
+    @Test
+    void testScenarioA() {
+        CartRequest request = new CartRequest(List.of(
+                new CartItem("A", 1),
+                new CartItem("B", 1),
+                new CartItem("C", 1)));
+        int total = promotionEngineService.calculateTotal(request);
+        assertEquals(100, total);
+    }
+}
