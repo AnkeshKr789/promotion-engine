978ce3be5879d98e401a891323ed651993102365
AnkeshKr789 <ankeshkumar7890@gmail.com>
2025-07-06 09:17:07 +0530

    feat: add Cart Item

---
+++ src/main/java/com/example/promotion_engine/model/CartItem.java
@@ -0,0 +1,13 @@
+package com.example.promotion_engine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    private String skuId;
+    private int quantity;
+}
