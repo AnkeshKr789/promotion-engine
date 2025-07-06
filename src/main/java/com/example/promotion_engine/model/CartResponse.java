525c8df4810c1421a190d258ba572b405f9c95fe
AnkeshKr789 <ankeshkumar7890@gmail.com>
2025-07-06 09:21:12 +0530

    feat: add Cart Response

---
+++ src/main/java/com/example/promotion_engine/model/CartResponse.java
@@ -0,0 +1,10 @@
+package com.example.promotion_engine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CartResponse {
+    private int total;
+}
